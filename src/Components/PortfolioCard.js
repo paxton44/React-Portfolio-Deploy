1bd3869fff8bb11ff9830afe8cc8d751f3949f12
Mitchell Flood <mitchflood1@gmail.com>
2021-05-30 10:42:51 -0600

    fixing last link hrefs

--- src/Components/PortfolioCard.js
+++ src/Components/PortfolioCard.js
@@ -99,8 +99,8 @@ const PortfolioCard = (props) => {
                                         <Card.Text>
                                         This homework assignment required the use of arrays, functions, conditional statements, and the application of a for loop. I first started by constructing arrays for uppercase, lowercase, numerical, and special characters. Then I grouped all four arrays into an empty array named allCharacters. I then modified the writePassword function. This function runs on the parameters of password length being between 8-128 characters. It has conditional statements that prompt and require the user to have at least 8 characters in their password, whether or not they want uppercase, lowercase, special, or numerical characters. It then generates the random password using the for loop to use characters from each array based on the user prompts."  
                                         </Card.Text>
-                                                <Button id='githubBtn' href='https://github.com/wylie-u/BeerNuts' variant="outline-primary">View GitHub Repo</Button>
-                                                <Button href='https://shrouded-reaches-33204.herokuapp.com/' variant="outline-danger">View Live Application</Button>
+                                                <Button id='githubBtn' href="https://github.com/paxton44/week-3-hw-fixed" variant="outline-primary">View GitHub Repo</Button>
+                                                <Button href='https://paxton44.github.io/week-3-hw-fixed/' variant="outline-danger">View Live Application</Button>
                                     </Card.Body>
                             </Card> 
                             </Col>  
